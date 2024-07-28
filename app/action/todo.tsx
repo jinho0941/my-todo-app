bc3f7c4e8348d33cd1551899283b06cf7d3f2324
daggax <daggax8235@gmail.com>
2024-07-28 20:07:42 +0900

    feat: 미리 해당 todo 의 CRUD에 대한 action들을 만들어 놓음,

--- app/action/todo.tsx
+++ app/action/todo.tsx
@@ -8,6 +8,64 @@ type ActionType = {
   message: string
 }
 
+export const createTodo = async (
+  title: string,
+  description: string,
+): Promise<ActionType> => {
+  try {
+    await api.post('/todos', { title, description })
+
+    revalidatePath('/')
+    revalidatePath('/server-component-fetch')
+    return { success: true, message: '성공' }
+  } catch (error) {
+    return { success: false, message: '실패' }
+  }
+}
+
+export const deleteTodo = async (todoId: string): Promise<ActionType> => {
+  try {
+    await api.delete(`/todos/${todoId}`)
+
+    revalidatePath('/')
+    revalidatePath('/server-component-fetch')
+    return { success: true, message: '성공' }
+  } catch (error) {
+    return { success: false, message: '실패' }
+  }
+}
+
+export const updateTodoCompletion = async (
+  todoId: string,
+): Promise<ActionType> => {
+  try {
+    await api.patch(`/todos/${todoId}/completed`, { completed: true })
+
+    revalidatePath('/')
+    revalidatePath('/server-component-fetch')
+    return { success: true, message: '성공' }
+  } catch (error) {
+    return { success: false, message: '실패' }
+  }
+}
+
+export const updateTodoDescription = async (
+  todoId: string,
+  description: string,
+): Promise<ActionType> => {
+  try {
+    await api.patch(`/todos/${todoId}/description`, {
+      description,
+    })
+
+    revalidatePath('/')
+    revalidatePath('/server-component-fetch')
+    return { success: true, message: '성공' }
+  } catch (error) {
+    return { success: false, message: '실패' }
+  }
+}
+
 export const updateTodoTitle = async (
   todoId: string,
   title: string,
@@ -15,6 +73,7 @@ export const updateTodoTitle = async (
   try {
     await api.patch(`/todos/${todoId}/title`, { title })
 
+    revalidatePath('/')
     revalidatePath('/server-component-fetch')
     return { success: true, message: '성공' }
   } catch (error) {
