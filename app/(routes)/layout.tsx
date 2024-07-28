784b6c067934473b7f662f21afad7bf9cfeb3852
daggax <daggax8235@gmail.com>
2024-07-28 19:09:01 +0900

    feat: routes layout 설정 및 route되는 페이지들 폴더, 파일 만들음.

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,12 @@
+import { Logo } from './_components/logo'
+
+const RoutesLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='bg-stone-200 min-h-screen text-black flex flex-col justify-center items-center'>
+      <Logo />
+      {children}
+    </div>
+  )
+}
+
+export default RoutesLayout
