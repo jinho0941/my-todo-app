0b3c4a6d47a9ef39f77e90c4a022a9bb6112cd7d
daggax <daggax8235@gmail.com>
2024-07-28 19:14:26 +0900

    feat: base fetch의 fetch함수 작성 및 데이터 불러오기 작성

--- app/(routes)/react-base-fetch/page.tsx
+++ app/(routes)/react-base-fetch/page.tsx
@@ -1,4 +1,33 @@
+'use client'
+
+import { api } from '@/app/utils'
+import { Todo } from '@/type'
+import { useEffect, useState } from 'react'
+
 const Page = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+  const [fetchTime, setFetchTime] = useState<number | null>(null)
+
+  const fetchTodos = async () => {
+    const startTime = performance.now()
+    try {
+      const response = await api.get<Todo[]>('/todos')
+      setTodoList(response.data)
+    } catch (error) {
+      setError('Error fetching todo list')
+    } finally {
+      const endTime = performance.now()
+      setFetchTime(endTime - startTime)
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
   return <div></div>
 }
 
