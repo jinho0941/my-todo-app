717e4770d991ba301e955da9c3f8c515ce7fa223
daggax <daggax8235@gmail.com>
2024-07-28 19:22:29 +0900

    feat: todo 완료 토글 구현 완료

---
+++ app/(routes)/react-base-fetch/_components/completion-todo-toggle.tsx
@@ -0,0 +1,42 @@
+import { useTransition } from 'react'
+import { api } from '@/app/utils'
+import { toast } from 'sonner'
+import { Loading } from '@/components/loading'
+
+type Props = {
+  id: string
+  isCompleted: boolean
+  onToggle: () => void
+}
+
+export const CompletionTodoToggle = ({ id, isCompleted, onToggle }: Props) => {
+  const [isPending, startTransition] = useTransition()
+
+  const handleCompletionToggle = () => {
+    startTransition(async () => {
+      try {
+        await api.patch(`/todos/${id}/completed`, { completed: !isCompleted })
+        onToggle()
+      } catch (error) {
+        toast.error('상태 변경에 실패하였습니다.')
+      }
+    })
+  }
+
+  return (
+    <>
+      {isPending && <Loading />}
+      <button
+        onClick={handleCompletionToggle}
+        className={`py-1 px-2 rounded-md border text-xs ${
+          isCompleted
+            ? 'bg-sky-500 text-white'
+            : 'bg-green-500 text-white hover:bg-green-600'
+        }`}
+        disabled={isCompleted}
+      >
+        {isCompleted ? '완료됨' : '완료하기'}
+      </button>
+    </>
+  )
+}
